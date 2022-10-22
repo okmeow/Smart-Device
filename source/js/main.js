e3936a3ef54e4e169738505be22accbb05cbc6dd
Ilia <ilytar@gmail.com>
2022-10-22 16:02:57 +0300

    Верстка модального окна

--- source/js/main.js
+++ source/js/main.js
@@ -41,6 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+
   });
 });
 
